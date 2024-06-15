7ff9816873766ddc7c56e80c564af69f9af28200
Jay Kumar Yadav <jay@crunchyfashion.com>
2024-06-15 13:13:27 +0530

    Image fix...

--- next.config.js
+++ next.config.js
@@ -4,17 +4,21 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      }
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "strapi-web-builder.onrender.com",
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
